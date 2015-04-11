10e7dbdd1684eb38e10ebbd82e1e9e25445f51ad
Isabel Porto <belporto@gmail.com>
2015-04-11 17:55:57 -0300

    Adicionando a simplify commerce no projeto

--- app/src/main/java/br/com/mastervoucher/activities/PaymentActivity.java
+++ app/src/main/java/br/com/mastervoucher/activities/PaymentActivity.java
@@ -1,40 +1,76 @@
 package br.com.mastervoucher.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+
+import com.simplify.android.sdk.Simplify;
+import com.simplify.android.sdk.model.Card;
+import com.simplify.android.sdk.model.SimplifyError;
+import com.simplify.android.sdk.model.Token;
+import com.simplify.android.sdk.view.CardEditor;
 
 import br.com.mastervoucher.R;
 
 public class PaymentActivity extends ActionBarActivity {
 
+    private static final String SIMPLIFY_KEY = "sbpb_NDhiODIyZTQtNzM4OC00YmRlLTliMzEtYjBmZWE5NjA5NDVh";
+    private static final String TAG = PaymentActivity.class.getSimpleName();
+    private Simplify mSimplify;
+    private CardEditor mCardEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        mSimplify = new Simplify(SIMPLIFY_KEY);
+        initUI();
     }
 
+    private void initUI() {
+        mCardEditor = (CardEditor) findViewById(R.id.card_editor);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_payment, menu);
-        return true;
+        mCardEditor.setOnChargeClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(view);
+
+                Card card = mCardEditor.getCard();
+                AsyncTask<?, ?, ?> task = mSimplify.createCardToken(card, new Simplify.CreateTokenListener() {
+                    @Override
+                    public void onSuccess(Token token) {
+                        Log.i(TAG, "Created Token: " + token.getId());
+                        mCardEditor.showSuccessOverlay("Created card token " + token.getId());
+                        //TODO: chamar a API do SAMIR
+                    }
+
+                    @Override
+                    public void onError(SimplifyError error) {
+                        Log.e(TAG, "Error Creating Token: " + error.getMessage());
+                        mCardEditor.showErrorOverlay("Unable to retrieve card token. " + error.getMessage());
+                    }
+                });
+            }
+        });
+
+        // init reset button
+        ((Button) findViewById(R.id.btnReset)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCardEditor.reset();
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
