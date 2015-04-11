f11935c9af1a0eeff2a61dc9ff3d8b5493a2d3f2
Alexandre Marones <alexandre.marones@gmail.com>
2015-04-11 20:06:49 -0300

    setting public

--- app/src/main/java/br/com/mastervoucher/models/ShopCartItem.java
+++ app/src/main/java/br/com/mastervoucher/models/ShopCartItem.java
@@ -6,8 +6,8 @@ import java.io.Serializable;
  * Created by Isabel Porto on 11/04/2015.
  */
 public class ShopCartItem implements Serializable {
-    private Product product;
-    private int quantity;
+    public Product product;
+    public int quantity;
 
     public double getTotalAmount(){
         double amount = product.getDoubleValue();
