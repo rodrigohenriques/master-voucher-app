c68bb43134a8fc523a57df59ee36990a05db8473
Isabel Porto <belporto@gmail.com>
2015-04-11 18:12:59 -0300

    Merge branch 'master' of https://github.com/rodrigohenriques/master-voucher-app
    Conflicts:
    	app/build.gradle
    	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/br/com/mastervoucher/service/PaymentService.java
@@ -0,0 +1,12 @@
+package br.com.mastervoucher.service;
+
+/**
+ * Created by Isabel Porto on 11/04/2015.
+ */
+public class PaymentService {
+
+    public void pay(String tokenId, String value){
+
+
+    }
+}
