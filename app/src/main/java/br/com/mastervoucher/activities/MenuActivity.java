df0df9e2f9f8f10762112fdf7181365383176776
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-04-11 22:45:07 -0300

    implemented menu list with headers

--- app/src/main/java/br/com/mastervoucher/activities/MenuActivity.java
+++ app/src/main/java/br/com/mastervoucher/activities/MenuActivity.java
@@ -7,17 +7,22 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.mastervoucher.R;
-import br.com.mastervoucher.adapters.MenuAdapter;
+import br.com.mastervoucher.adapters.menulist.Header;
+import br.com.mastervoucher.adapters.menulist.Item;
+import br.com.mastervoucher.adapters.menulist.ListItem;
+import br.com.mastervoucher.adapters.menulist.MenuListAdapter;
+import br.com.mastervoucher.models.Product;
 import br.com.mastervoucher.models.ShopCart;
+import br.com.mastervoucher.models.ShopCartItem;
 import br.com.mastervoucher.util.Extras;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 
-/**
- * Created by AlexandreMarones on 4/11/15.
- */
 public class MenuActivity extends BaseActivity {
 
     @InjectView(R.id.listview)
@@ -38,8 +43,21 @@ public class MenuActivity extends BaseActivity {
     }
 
     private void setupListViewContent() {
-        MenuAdapter menuAdapter = new MenuAdapter(this, R.layout.adapter_menu, shopCart.getShopCartItems());
-        listview.setAdapter(menuAdapter);
+        //MenuAdapter menuAdapter = new MenuAdapter(this, R.layout.adapter_menu, shopCart.getShopCartItems());
+
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Header("Header 1"));
+        items.add(new ListItem(new ShopCartItem(new Product(), 5)));
+        items.add(new ListItem(new ShopCartItem(new Product(), 11)));
+        items.add(new Header("Header 2"));
+        items.add(new ListItem(new ShopCartItem(new Product(), 2)));
+        items.add(new ListItem(new ShopCartItem(new Product(), 3)));
+        items.add(new ListItem(new ShopCartItem(new Product(), 12)));
+        items.add(new ListItem(new ShopCartItem(new Product(), 5)));
+        items.add(new ListItem(new ShopCartItem(new Product(), 8)));
+
+        MenuListAdapter adapter = new MenuListAdapter(this, items);
+        listview.setAdapter(adapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
