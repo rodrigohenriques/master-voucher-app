3f6dce360a264f0fa3acc823d08e2f5256fe99a0
Rodrigo Henriques <rodrigo.henriques@m4u.com.br>
2015-04-12 06:06:23 -0300

    implemented delivery info to qr code

--- app/src/main/java/br/com/mastervoucher/util/QRCodeUtil.java
+++ app/src/main/java/br/com/mastervoucher/util/QRCodeUtil.java
@@ -1,21 +1,16 @@
 package br.com.mastervoucher.util;
 
 
+import android.graphics.Bitmap;
+
 import net.glxn.qrgen.android.QRCode;
-
-import java.io.File;
-import java.util.List;
-
-import br.com.mastervoucher.models.ShopCartItem;
 
 /**
  * Created by Isabel Porto on 12/04/2015.
  */
 public class QRCodeUtil {
 
-    public File generateQRCodeBy(List<ShopCartItem> shopCartItems) {
-        JSONUtil jsonUtil = new JSONUtil();
-        String string = jsonUtil.to(shopCartItems);
-        return QRCode.from(string).file();
+    public Bitmap generateQRCodeBy(String string) {
+        return QRCode.from(string).bitmap();
     }
 }
